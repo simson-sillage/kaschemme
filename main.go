aceb6948fd9a3f570aefe7d2074738401ac140ac
Simon Schricker <s.schricker@sillage.at>
2025-02-01 20:48:55 +0100

    minimal tcp forwarder

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"net"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	localAddr := ":8080"
+	remoteAddr := "localhost:8081"
+	listener, err := net.Listen("tcp", localAddr)
+	if err != nil {
+		log.Fatalf("failed to bind to %s: %s\n", localAddr, err)
+	}
+	defer listener.Close()
+	log.Printf("Listening on: %s\n", localAddr)
+	log.Printf("Forwarding to: %s\n", remoteAddr)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Connection error: %s\n", err)
+			continue
+		}
+		go handleConnection(conn, remoteAddr)
+	}
+}
+
+func handleConnection(src net.Conn, destAddr string) {
+	defer src.Close()
+	log.Printf("handling connection for %s\n", src.RemoteAddr())
+	dst, err := net.Dial("tcp", destAddr)
+	if err != nil {
+		log.Printf("Failed to connect to remote: %s: %s\n", destAddr, err)
+		return
+	}
+	defer dst.Close()
+	go io.Copy(dst, src)
+	io.Copy(src, dst)
 }
