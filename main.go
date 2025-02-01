a4dba695cddd94c4a3f04f4ddd98aaadc55fca5b
Simon Schricker <s.schricker@sillage.at>
2025-02-01 21:04:08 +0100

    add error handling for io.Copy()
    This is still broken, because the conn can be closed by one of the
    transfer() while the other is still active.
    
    Creating following error: error copying from 127.0.0.1:51320 to [::1]:8081: writeto tcp 127.0.0.1:8080->127.0.0.1:51320: readfrom tcp [::1]:51321->[::1]:8081: read tcp 127.0.0.1:8080->127.0.0.1:51320: use of closed network connection
    
    Maybe use channels to fix this?

--- main.go
+++ main.go
@@ -36,6 +36,22 @@ func handleConnection(src net.Conn, destAddr string) {
 		return
 	}
 	defer dst.Close()
-	go io.Copy(dst, src)
-	io.Copy(src, dst)
+	go transfer(src, dst)
+	transfer(dst, src)
+
+}
+
+func transfer(src net.Conn, dst net.Conn) {
+	n, err := io.Copy(dst, src)
+	if err != nil {
+		log.Printf("error copying from %s to %s: %s\n",
+			src.RemoteAddr().String(),
+			dst.RemoteAddr().String(),
+			err)
+		return
+	}
+	go log.Printf("wrote %d bytes from %s to %s\n",
+		n,
+		src.RemoteAddr().String(),
+		dst.RemoteAddr().String())
 }
