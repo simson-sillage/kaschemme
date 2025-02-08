39469d98db9ce8fd20b867c3d727c8584e0c9d7c
Simon Schricker <s.schricker@sillage.at>
2025-02-08 19:56:37 +0100

    add: query all sentinels for master
    - using github.com/redis/go-redis/v9 library
    - master with most votes by sentinels wins

--- go.mod
+++ go.mod
@@ -6,7 +6,11 @@ require github.com/spf13/cobra v1.8.1
 
 require gopkg.in/yaml.v3 v3.0.1
 
+require github.com/redis/go-redis/v9 v9.7.0
+
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
--- redis.go
+++ redis.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 
+	"github.com/redis/go-redis/v9"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 )
 
 type RedisConfig struct {
-	Sentinels []string `yaml:"sentinels"`
+	Sentinels    []string `yaml:"sentinels"`
+	RedisPass    string   `yaml:"redis_pass"`
+	SentinelPass string   `yaml:"sentinel_pass"`
+	MasterName   string   `yaml:"master_name"`
 }
 
 func redisMode(cmd *cobra.Command, args []string) {
@@ -33,5 +38,38 @@ func redisMode(cmd *cobra.Command, args []string) {
 	}
 	config := RedisConfig{}
 	yaml.Unmarshal(rawConfig, &config)
-	fmt.Println(config.Sentinels)
+	fmt.Println("sentinels:", config.Sentinels)
+	fmt.Println("redis pass:", config.RedisPass)
+	fmt.Println("sentinel pass:", config.SentinelPass)
+	fmt.Println("master name:", config.MasterName)
+
+	ctx := context.TODO()
+	masterVoting := make(map[string]int)
+	for _, addr := range config.Sentinels {
+		sentinel := redis.NewSentinelClient(&redis.Options{
+			Addr: addr,
+		})
+		defer sentinel.Close()
+		response, err := sentinel.GetMasterAddrByName(ctx, config.MasterName).Result()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		masterAddr := net.JoinHostPort(response[0], response[1])
+		masterVoting[masterAddr] += 1
+	}
+	master := findMaster(masterVoting)
+	fmt.Println("master:", master)
+}
+
+func findMaster(masterVoting map[string]int) string {
+	currentMaster := ""
+	currentCount := 0
+	for master, count := range masterVoting {
+		if count > currentCount {
+			currentMaster = master
+			currentCount = count
+		}
+	}
+
+	return currentMaster
 }
