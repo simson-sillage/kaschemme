5c781bf24672962a4197a79f03b1633716a27ceb
Simon Schricker <s.schricker@sillage.at>
2025-02-09 14:32:14 +0100

    implement caching and async update of redismaster

--- redis.go
+++ redis.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net"
 	"os"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -14,10 +16,11 @@ import (
 )
 
 type RedisConfig struct {
-	Sentinels       []string `yaml:"sentinels"`
-	SentinelPass    string   `yaml:"sentinel_pass"`
-	SentinelTimeout int      `yaml:"sentinel_timeout"`
-	MasterName      string   `yaml:"master_name"`
+	Sentinels          []string `yaml:"sentinels"`
+	SentinelPass       string   `yaml:"sentinel_pass"`
+	SentinelTimeout    int      `yaml:"sentinel_timeout"`
+	MasterName         string   `yaml:"master_name"`
+	MasterCacheTimeout int      `yaml:"master_cache_timeout"`
 }
 
 func redisMode(cmd *cobra.Command, args []string) {
@@ -37,7 +40,11 @@ func redisMode(cmd *cobra.Command, args []string) {
 	if err != nil {
 		log.Fatalf("error reading config file at %s: %s\n", configPath, err)
 	}
-	config := RedisConfig{}
+	config := RedisConfig{
+		SentinelTimeout:    5,
+		MasterCacheTimeout: 5,
+		SentinelPass:       "",
+	}
 	yaml.Unmarshal(rawConfig, &config)
 	for _, sentinel := range config.Sentinels {
 		if _, err := net.ResolveTCPAddr("tcp", sentinel); err != nil {
@@ -53,26 +60,59 @@ func redisMode(cmd *cobra.Command, args []string) {
 	log.Printf("Listening on: %s\n", localAddr)
 	log.Printf("Configured Sentinels: %s\n", strings.Join(config.Sentinels, ", "))
 
+	var ptrMaster atomic.Pointer[string]
+	updateMaster(&ptrMaster, config)
+	go updateMasterPeriodically(&ptrMaster, config)
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Printf("Connection error: %s\n", err)
 			continue
 		}
-		go redisHandleConnection(conn, config)
+		go redisHandleConnection(conn, &ptrMaster)
 	}
 }
 
-func redisHandleConnection(src net.Conn, config RedisConfig) {
-	candidates := getMasterCandidates(config)
-	if len(candidates) <= 0 {
-		log.Printf("error: couldn't determine master\n")
+func updateMaster(ptrMaster *atomic.Pointer[string], config RedisConfig) {
+	master, votes, err := findMaster(config)
+	if err != nil {
+		log.Printf("error: %s\n", err)
+		master = ""
+		ptrMaster.Store(&master)
+		return
+	}
+	ptrMaster.Store(&master)
+	log.Printf("current master: %s, votes: %d\n", master, votes)
+}
+
+func updateMasterPeriodically(ptrMaster *atomic.Pointer[string], config RedisConfig) {
+	cacheTimeout := time.Duration(config.MasterCacheTimeout) * time.Second
+	log.Printf("trying to update master every %d seconds\n", config.MasterCacheTimeout)
+	for {
+		updateMaster(ptrMaster, config)
+		time.Sleep(cacheTimeout)
+	}
+}
+
+func redisHandleConnection(src net.Conn, ptrMaster *atomic.Pointer[string]) {
+	master := *ptrMaster.Load()
+	if master == "" {
+		log.Printf("error: can't handle connection from %s, master unavailable", src.RemoteAddr().String())
 		return
 	}
 
-	master, votes := findMaster(candidates)
 	go handleConnection(src, master)
-	log.Printf("current master: %s, votes: %d\n", master, votes)
+}
+
+func findMaster(config RedisConfig) (string, int, error) {
+	candidates := getMasterCandidates(config)
+	if len(candidates) <= 0 {
+		return "", 0, errors.New("couldn't determine master")
+	}
+
+	master, votes := countMasterVotes(candidates)
+	return master, votes, nil
 }
 
 func getMasterCandidates(config RedisConfig) map[string]int {
@@ -100,7 +140,7 @@ func getMasterCandidates(config RedisConfig) map[string]int {
 	return candidates
 }
 
-func findMaster(masterVoting map[string]int) (string, int) {
+func countMasterVotes(masterVoting map[string]int) (string, int) {
 	currentMaster := ""
 	currentCount := 0
 	for master, count := range masterVoting {
